e9ce3cb1685fdf620d84f00a850aabf90defb57e
Michal Ungeheuer <wbsth@users.noreply.github.com>
2019-08-10 21:44:48 +0200

    completed first exercise

--- part01-e01_hello_world/src/hello_world.py
+++ part01-e01_hello_world/src/hello_world.py
@@ -11,6 +11,7 @@
 
 def main():
     # Enter your code here, this is the correct indentation
+    print("Hello, world!")
 
 # This makes sure the main function is not called immediatedly
 # when TMC imports this module
