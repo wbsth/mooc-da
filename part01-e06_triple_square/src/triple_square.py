8f7c74dabcd6d78eaa6f0fbf1fc3da4c1ff8d171
Michal Ungeheuer <wbsth@users.noreply.github.com>
2019-08-10 22:33:52 +0200

    completed exercise

--- part01-e06_triple_square/src/triple_square.py
+++ part01-e06_triple_square/src/triple_square.py
@@ -1,8 +1,20 @@
 #!/usr/bin/env python3
 
+def triple(number):
+    return number * 3
+
+def square(number):
+    return number ** 2
 
 def main():
-    pass
+    for i in range(1, 11):
+        tripled = triple(i)
+        squared = square(i)
+        if squared > tripled:
+            break
+        else:
+            print(f"triple({i})=={tripled} square({i})=={squared}")
+        
 
 if __name__ == "__main__":
     main()
