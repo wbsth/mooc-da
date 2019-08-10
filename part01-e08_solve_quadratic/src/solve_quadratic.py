21bc0eee364844645415cb715da97320d48d7747
Michal Ungeheuer <wbsth@users.noreply.github.com>
2019-08-10 23:34:11 +0200

    completed exercise

--- part01-e08_solve_quadratic/src/solve_quadratic.py
+++ part01-e08_solve_quadratic/src/solve_quadratic.py
@@ -3,11 +3,15 @@
 import math
 
 def solve_quadratic(a, b, c):
-    return (0,0)
+    x1 = (-b - math.sqrt(b**2 - 4*a*c)) / (2*a)
+    x2 = (-b + math.sqrt(b**2 - 4*a*c)) / (2*a)
+    return((x1,x2))
 
 
 def main():
-    pass
+    print(solve_quadratic(1,-3,2))
+    print(solve_quadratic(1,2,1))
+
 
 if __name__ == "__main__":
     main()
