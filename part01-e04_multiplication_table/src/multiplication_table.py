496b3500516f14a8b7f29806c142339139fc23db
Michal Ungeheuer <wbsth@users.noreply.github.com>
2019-08-10 22:02:11 +0200

    completed exercise

--- part01-e04_multiplication_table/src/multiplication_table.py
+++ part01-e04_multiplication_table/src/multiplication_table.py
@@ -2,7 +2,11 @@
 
 
 def main():
-    pass
+    for i in range(1, 11):
+        for j in range(1, 11):
+            print(f"{i*j:4d}", end = ' ')
+        print("")
+
 
 if __name__ == "__main__":
     main()
