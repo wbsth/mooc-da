df1c415820739682c46023cbf68820fbadd4311a
Michal Ungeheuer <wbsth@users.noreply.github.com>
2019-08-10 21:48:55 +0200

    2nd exercise completed

--- part01-e02_compliment/src/compliment.py
+++ part01-e02_compliment/src/compliment.py
@@ -2,8 +2,8 @@
 
 
 def main():
-    # Enter you solution here
-
+    country = input("What country are you from? ")
+    print(f"I have heard that {country} is a beautiful country.")
 
 if __name__ == "__main__":
     main()
