cac8c2480b3be44eb4a933c54b8eda5c8c7cb3fe
Michal Ungeheuer <wbsth@users.noreply.github.com>
2019-08-11 00:01:21 +0200

    completed exercise

--- part01-e09_merge/src/merge.py
+++ part01-e09_merge/src/merge.py
@@ -1,10 +1,28 @@
 #!/usr/bin/env python3
 
 def merge(L1, L2):
-    return []
+    size1 = len(L1)
+    size2 = len(L2)
+    
+    i, j = 0, 0
+    result = []
+
+    while i < size1 and j <size2:
+        if L1[i] < L2[j]:
+            result.append(L1[i])
+            i += 1
+        else:
+            result.append(L2[j])
+            j += 1
+    
+    # adding leftovers
+    result = result + L1[i:] + L2[j:]
+    return result
 
 def main():
-    pass
+    L1 = [1,5,9,12]
+    L2 = [2,6,10]
+    print(merge(L1, L2))
 
 if __name__ == "__main__":
     main()
