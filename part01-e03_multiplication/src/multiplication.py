e22a8631a612edf34d77fc4a136eae4892249a20
Michal Ungeheuer <wbsth@users.noreply.github.com>
2019-08-10 21:51:16 +0200

    3rd exercise completed

--- part01-e03_multiplication/src/multiplication.py
+++ part01-e03_multiplication/src/multiplication.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python3
 
 def main():
-    # Enter your solution here
+    for i in range(11):
+        print(f"4 multiplied by {i} is {4*i}")
 
 if __name__ == "__main__":
     main()
