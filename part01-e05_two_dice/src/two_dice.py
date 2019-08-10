806b45be9e5bdbd68271052588895e3b97aefce4
Michal Ungeheuer <wbsth@users.noreply.github.com>
2019-08-10 22:23:16 +0200

    completed exercise

--- part01-e05_two_dice/src/two_dice.py
+++ part01-e05_two_dice/src/two_dice.py
@@ -1,7 +1,10 @@
 #!/usr/bin/env python3
 
 def main():
-    pass
+    for i in range(1,7):
+        for j in range(1,7):
+            if i+j == 5:
+                print(f"({i},{j})")
 
 if __name__ == "__main__":
     main()
