a43ee4e2276346a5dbd800ff13df605935f5ba59
Michal Ungeheuer <wbsth@users.noreply.github.com>
2019-08-11 23:40:48 +0200

    completed exercise

--- part01-e11_interleave/src/interleave.py
+++ part01-e11_interleave/src/interleave.py
@@ -1,7 +1,16 @@
 #!/usr/bin/env python3
 
 def interleave(*lists):
-    return []
+    lst = []
+    lst2 = []
+    for i in lists:
+        lst.append(i)
+    zipped = list(zip(*lst))
+    for i in zipped:
+        for j in i:
+            lst2.append(j)
+    return lst2
+    
 
 def main():
     print(interleave([1, 2, 3], [20, 30, 40], ['a', 'b', 'c']))
