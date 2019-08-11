63ce7816f1aec86e34d71e4b3cbb7b6d1da578f9
Michal Ungeheuer <wbsth@users.noreply.github.com>
2019-08-12 00:21:36 +0200

    completed exercise

--- part01-e13_reverse_dictionary/src/reverse_dictionary.py
+++ part01-e13_reverse_dictionary/src/reverse_dictionary.py
@@ -1,7 +1,16 @@
 #!/usr/bin/env python3
 
 def reverse_dictionary(d):
-    return {}
+    new_dict = {}
+    
+    for i in d:
+        for j in d[i]:
+            try:
+                new_dict[j].append(i)
+            except:
+                new_dict[j] = [i]
+
+    return(new_dict)
 
 def main():
     pass
