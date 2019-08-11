b06cd030bc62a2682aed8e507a0be85710eae9a0
Michal Ungeheuer <wbsth@users.noreply.github.com>
2019-08-11 23:54:40 +0200

    completed exercise

--- part01-e12_distinct_characters/src/distinct_characters.py
+++ part01-e12_distinct_characters/src/distinct_characters.py
@@ -1,7 +1,12 @@
 #!/usr/bin/env python3
 
 def distinct_characters(L):
-    return {}
+    res = {}
+    for word in L:
+        dist_chars = len(set(word))
+        res[word] = dist_chars
+    return res
+
 
 def main():
     print(distinct_characters(["check", "look", "try", "pop"]))
