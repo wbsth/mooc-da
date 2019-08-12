ecedfea2bc141438aa3efeb972da2460e8733184
Michal Ungeheuer <wbsth@users.noreply.github.com>
2019-08-12 13:15:32 +0200

    completed exercise

--- part01-e14_find_matching/src/find_matching.py
+++ part01-e14_find_matching/src/find_matching.py
@@ -1,7 +1,13 @@
 #!/usr/bin/env python3
 
 def find_matching(L, pattern):
-    return []
+    res = []
+    for cnt, val in enumerate(L):
+        if pattern in val:
+            res.append(cnt)
+
+    return res
+
 
 def main():
     pass
