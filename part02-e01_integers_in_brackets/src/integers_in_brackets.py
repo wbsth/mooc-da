8ef600dfc71edc4d1f5d6fb32ad75bae4e5e310c
Michal Ungeheuer <wbsth@users.noreply.github.com>
2019-08-12 22:27:28 +0200

    completed exercise p2-1

--- part02-e01_integers_in_brackets/src/integers_in_brackets.py
+++ part02-e01_integers_in_brackets/src/integers_in_brackets.py
@@ -1,8 +1,9 @@
 #!/usr/bin/env python3
-
+import re
 
 def integers_in_brackets(s):
-    return []
+    test = re.findall('\[\s*([\+\-]?\d+)\s*\]', s)
+    return [int(i) for i in test]
 
 def main():
     pass
