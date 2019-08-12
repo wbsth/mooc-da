d2894ef8f0c657e06e80be04879a43d1a18a866d
Michal Ungeheuer <wbsth@users.noreply.github.com>
2019-08-12 13:21:52 +0200

    completed exercise

--- part01-e15_two_dice_comprehension/src/two_dice_comprehension.py
+++ part01-e15_two_dice_comprehension/src/two_dice_comprehension.py
@@ -1,7 +1,9 @@
 #!/usr/bin/env python3
 
 def main():
-    pass
+    res = [(x,y) for x in range(1,7) for y in range(1,7) if x+y == 5]
+    for i in res:
+        print(i)
 
 if __name__ == "__main__":
     main()
