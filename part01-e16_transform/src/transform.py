7e20908a4035cbf3624d28edb2fdaa5d5276d78e
Michal Ungeheuer <wbsth@users.noreply.github.com>
2019-08-12 13:30:32 +0200

    completed exercise

--- part01-e16_transform/src/transform.py
+++ part01-e16_transform/src/transform.py
@@ -1,10 +1,20 @@
 #!/usr/bin/env python3
 
 def transform(s1, s2):
-    return []
+    result = []
+    s1 = s1.split()
+    s2 = s2.split()
+    list1 = list(map(int, s1))
+    list2 = list(map(int, s2))
+
+    zipped = zip(list1, list2)
+    for i in list(zipped):
+        result.append(i[0] * i[1])
+
+    return(result)
 
 def main():
-    pass
+    transform("1 5 3", "2 6 -1")
 
 if __name__ == "__main__":
     main()
