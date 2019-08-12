931e692fa38bd62b02e780d3f5afd1af7bda44a6
Michal Ungeheuer <wbsth@users.noreply.github.com>
2019-08-12 14:33:54 +0200

    completed exercise19

--- part01-e19_sum_equation/src/sum_equation.py
+++ part01-e19_sum_equation/src/sum_equation.py
@@ -1,10 +1,15 @@
 #!/usr/bin/env python3
 
 def sum_equation(L):
-    return ""
+    if len(L) == 0:
+        return('0 = 0')
+    else:
+        numb_sum = sum(L)
+        L_strings = [str(i) for i in L]
+        return (' + '.join(L_strings) + ' = ' + str(numb_sum))
 
 def main():
-    pass
+    print(sum_equation([]))
 
 if __name__ == "__main__":
     main()
