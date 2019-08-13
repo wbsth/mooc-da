cfae3210ca538db937577ba23ab6e1bd43371196
Michal Ungeheuer <wbsth@users.noreply.github.com>
2019-08-14 00:44:16 +0200

    completed exercise p2-4

--- part02-e04_word_frequencies/src/word_frequencies.py
+++ part02-e04_word_frequencies/src/word_frequencies.py
@@ -1,7 +1,21 @@
 #!/usr/bin/env python3
 
 def word_frequencies(filename):
-    return {}
+    wordlist = []
+    cnt_dictio = {}
+    with open(filename, 'r') as f:
+        for line in f:
+            splitted = line.split()
+            stripped = [i.strip("""!"#$%&'()*,-./:;?@[]_""") for i in splitted]
+            for i in stripped:
+                wordlist.append(i)
+    wordset = set(wordlist)
+    for i in wordset:
+        cnt = wordlist.count(i)
+        cnt_dictio[i] = cnt
+
+    return cnt_dictio
+
 
 def main():
     pass
