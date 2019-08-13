81ccc4ba39ad34657b09b99b3ffb5b28a12c5e0e
Michal Ungeheuer <wbsth@users.noreply.github.com>
2019-08-14 00:16:47 +0200

    completed exercise 2-2

--- part02-e02_file_listing/src/file_listing.py
+++ part02-e02_file_listing/src/file_listing.py
@@ -4,7 +4,14 @@ import re
 
 
 def file_listing(filename="src/listing.txt"):
-    return []
+    res = []
+    expression = r'(\d+)\s+(\w+)\s+(\d+)\s+(\d+):(\d+)\s(.+)'
+    with open(filename, 'r') as f:
+        for i in f:
+            size, month, day, hour, minute, filename = re.search(expression, i).groups()
+            res.append((int(size), month, int(day), int(hour), int(minute), filename))
+    return res     
+
 
 def main():
     pass
