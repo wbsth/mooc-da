fd93adf81d9bda0fc51d8221b35ee08f2e1d204a
Michal Ungeheuer <wbsth@users.noreply.github.com>
2019-08-14 13:46:59 +0200

    completed exercies p2-7

--- part02-e07_file_extensions/src/file_extensions.py
+++ part02-e07_file_extensions/src/file_extensions.py
@@ -1,10 +1,30 @@
 #!/usr/bin/env python3
 
 def file_extensions(filename):
-    return ([], {})
+    with open(filename, 'r') as f:
+        no_ext = []
+        ext_dict = {}
+        for i in f:
+            i = i.rstrip()
+            if not '.' in i:
+                no_ext.append(i)
+            else:
+                splitted = i.split('.')
+                filename = splitted[0]
+                extension = splitted[-1]
+                try:
+                    ext_dict[extension].append(i)
+                except KeyError:
+                    ext_dict[extension] = [i]
+
+    return (no_ext, ext_dict)
 
 def main():
-    pass
+    no_ext, ext_dict = file_extensions('src/filenames.txt')
+    print(f'{len(no_ext)} files with no extension')
+    for key, value in ext_dict.items():
+        print(f'{key} {len(value)}')
+        
 
 if __name__ == "__main__":
     main()
