0653229ad333a0a28b46ce626a70433758b04164
Michal Ungeheuer <wbsth@users.noreply.github.com>
2019-08-14 11:38:48 +0200

    completed exercise p2-5

--- part02-e05_summary/src/summary.py
+++ part02-e05_summary/src/summary.py
@@ -1,12 +1,26 @@
 #!/usr/bin/env python3
 
 import sys
+import statistics
 
 def summary(filename):
-    return (0,0,0)
+    numb_list = []
+    with open(filename, 'r') as f:
+        for i in f:
+            try:
+                numb_list.append(float(i))
+            except:
+                pass  
+    sm = sum(numb_list)
+    av = statistics.mean(numb_list)
+    std = statistics.stdev(numb_list)    
+    return (sm, av, std)
 
 def main():
-    pass
+    files = sys.argv[1:]
+    for i in files:
+        sm, av, std = summary(i)
+        print(f"File: {i} Sum: {sm:.6f} Average: {av:.6f} Stddev: {std:.6f}")
 
 if __name__ == "__main__":
     main()
