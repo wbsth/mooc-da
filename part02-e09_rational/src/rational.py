f691fb81a29296386ef2e248bd8ba9d97a264c99
Michal Ungeheuer <wbsth@users.noreply.github.com>
2019-08-14 15:04:43 +0200

    completed exercise p2-9

--- part02-e09_rational/src/rational.py
+++ part02-e09_rational/src/rational.py
@@ -1,7 +1,53 @@
 #!/usr/bin/env python3
 
 class Rational(object):
-    pass
+    def __init__(self, numerator, denominator):
+        self.numerator = numerator
+        self.denominator = denominator
+
+    def __str__(self):
+        return f'{self.numerator}/{self.denominator}'
+
+    def __add__(self, numb):
+        new_denom = int(self.denominator) * int(numb.denominator)
+        mult1 = new_denom / self.denominator
+        mult2 = new_denom / numb.denominator
+        new_nume = int(mult1 * int(self.numerator) + mult2 * int(numb.numerator))
+        return Rational(new_nume, new_denom)
+
+    def __sub__(self, numb):
+        new_denom = int(self.denominator) * int(numb.denominator)
+        mult1 = new_denom / self.denominator
+        mult2 = new_denom / numb.denominator
+        new_nume = int(mult1 * int(self.numerator) - mult2 * int(numb.numerator))
+        return Rational(new_nume, new_denom)
+
+    def __mul__(self, numb):
+        #multiplication
+        new_denom = int(self.denominator * numb.denominator)
+        new_nume = int(self.numerator * numb.numerator)
+        return Rational(new_nume, new_denom)
+
+    def __truediv__(self, numb):
+        new_nume = int(self.numerator * numb.denominator)
+        new_denom = int(self.denominator * numb.numerator)
+        return Rational(new_nume, new_denom)
+
+    def __eq__(self, numb):
+        return self.numerator == numb.numerator and self.denominator == numb.denominator
+
+    def __gt__(self, numb):
+        new_denom = int(self.denominator) * int(numb.denominator)
+        mult1 = new_denom / self.denominator
+        mult2 = new_denom / numb.denominator
+        return mult1 * self.numerator > mult2 * self.denominator
+
+    def __lt__(self, numb):
+        new_denom = int(self.denominator) * int(numb.denominator)
+        mult1 = new_denom / self.denominator
+        mult2 = new_denom / numb.denominator
+        return mult1 * self.numerator < mult2 * self.denominator
+
 
 def main():
     r1=Rational(1,4)
