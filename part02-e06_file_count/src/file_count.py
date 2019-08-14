035d50263816e2826666c0eb6542ecdf818b2104
Michal Ungeheuer <wbsth@users.noreply.github.com>
2019-08-14 12:05:21 +0200

    completed exercise p2-6

--- part02-e06_file_count/src/file_count.py
+++ part02-e06_file_count/src/file_count.py
@@ -3,10 +3,23 @@
 import sys
 
 def file_count(filename):
-    return (0, 0, 0)
+    with open(filename, 'r') as f:
+        line_count, words_count, chars_count = 0, 0, 0
+
+        for line in f:
+            line_count += 1
+            chars_count += len(line)
+            words = line.split()
+            words_count += len(words)
+        
+        return(line_count, words_count, chars_count)
 
 def main():
-    pass
+    file_list = sys.argv[1:]
+    
+    for i in file_list:
+        lines, words, chars = file_count(i)
+        print(f'{lines}\t{words}\t{chars}\t{i}')
 
 if __name__ == "__main__":
     main()
