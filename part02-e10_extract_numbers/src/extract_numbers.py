288afae63be51b289d7e63d14c77f92b4412852d
Michal Ungeheuer <wbsth@users.noreply.github.com>
2019-08-14 16:03:38 +0200

    completed exercise p2-10

--- part02-e10_extract_numbers/src/extract_numbers.py
+++ part02-e10_extract_numbers/src/extract_numbers.py
@@ -1,7 +1,18 @@
 #!/usr/bin/env python3
 
 def extract_numbers(s):
-    return []
+    ret_list = []
+    splitted = s.split()
+    for word in splitted:
+        try:
+            ret_list.append(int(word))
+        except:
+            try:
+                ret_list.append(float(word))
+            except:
+                pass
+
+    return ret_list
 
 def main():
     print(extract_numbers("abd 123 1.2 test 13.2 -1"))
