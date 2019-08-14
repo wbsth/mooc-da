2f63a4a355299899dd5a17367a04e5602872529e
Michal Ungeheuer <wbsth@users.noreply.github.com>
2019-08-14 14:22:41 +0200

    completed exercise p2-8

--- part02-e08_prepend/src/prepend.py
+++ part02-e08_prepend/src/prepend.py
@@ -1,10 +1,16 @@
 #!/usr/bin/env python3
 
 class Prepend(object):
-    # Add the methods of the class here
+    def __init__(self, s):
+        self.s = s
+
+    def write(self, text):
+        print(f'{self.s}{text}')
+        
 
 def main():
-    pass
+    p = Prepend("+++ ")
+    p.write("Hello")
 
 if __name__ == "__main__":
     main()
