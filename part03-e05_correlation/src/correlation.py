1f3d0573656ae165ddd73506b6d95d964bbdbba7
Michal Ungeheuer <wbsth@users.noreply.github.com>
2019-08-28 22:46:12 +0200

    new exercises from part3 added

---
+++ part03-e05_correlation/src/correlation.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+
+import scipy.stats
+import numpy as np
+
+
+def load2():
+    """This loads the data from the internet. Does not work well on the TMC server."""
+    import seaborn as sns
+    return sns.load_dataset('iris').drop('species', axis=1).values
+
+def load():
+    import pandas as pd
+    return pd.read_csv("src/iris.csv").drop('species', axis=1).values
+
+def lengths():
+    return 0
+
+def correlations():
+    return np.array([])
+
+def main():
+    print(lengths())
+    print(correlations())
+
+if __name__ == "__main__":
+    main()
