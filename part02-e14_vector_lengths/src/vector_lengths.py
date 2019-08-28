88a0a951183bc7dd9230b047b49ebf457fde7f73
Michal Ungeheuer <wbsth@users.noreply.github.com>
2019-08-28 22:41:45 +0200

    completed exercise p2-14

--- part02-e14_vector_lengths/src/vector_lengths.py
+++ part02-e14_vector_lengths/src/vector_lengths.py
@@ -4,10 +4,11 @@ import numpy as np
 #import scipy.linalg
 
 def vector_lengths(a):
-    return np.array([])
+    return np.sqrt((a**2).sum(axis=1))
 
 def main():
-    pass
+    test_array = np.array([[1,2,3], [4,5,6]])
+    vector_lengths(test_array)
 
 if __name__ == "__main__":
     main()
