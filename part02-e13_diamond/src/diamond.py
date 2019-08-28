0bbe778d1a6b5f35dd06d34ea873c21a100f2a07
Michal Ungeheuer <wbsth@users.noreply.github.com>
2019-08-28 21:57:32 +0200

    completed exercise p2-13

--- part02-e13_diamond/src/diamond.py
+++ part02-e13_diamond/src/diamond.py
@@ -3,7 +3,14 @@
 import numpy as np
 
 def diamond(n):
-    return np.array([])
+    q1 = np.fliplr(np.eye(n, dtype = int))
+    q2 = np.eye(n, n-1, k = -1, dtype = int)
+    q3 = np.eye(n-1, n, k = 1, dtype = int)
+    q4 = np.fliplr(np.eye(n-1, k = 1, dtype = int))
+
+    upper = np.concatenate((q1, q2), axis = 1)
+    lower = np.concatenate((q3, q4), axis = 1)
+    return (np.concatenate((upper, lower)))
 
 def main():
     pass
