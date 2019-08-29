fd66ceba64d2aa805a11ed02c317b5f9d6a94aee
Michal Ungeheuer <wbsth@users.noreply.github.com>
2019-08-29 16:33:18 +0200

    completed exercise p2-15

--- part02-e15_vector_angles/src/vector_angles.py
+++ part02-e15_vector_angles/src/vector_angles.py
@@ -4,10 +4,23 @@ import numpy as np
 import scipy.linalg
 
 def vector_angles(X, Y):
-    np.array([])
+    length = X.shape[0]
+    res = np.zeros((length))
+
+    for i in range(length):
+        res[i] = (np.dot(X[i], Y[i])/scipy.linalg.norm(X[i])/scipy.linalg.norm(Y[i]))
+
+    print(f"Przed clipowaniem: {res}")
+    clipped = np.clip(res, -1, 1)
+    print(f"Po clipowaniu: {clipped}")
+    angles = np.arccos(clipped)
+    return np.degrees(angles)
+
 
 def main():
-    np.array([])
+    a = np.array([[0,0,1],[-1,1,0]])
+    b = np.array([[0,1,0], [1,1,0]])
+    print(vector_angles(a, b))
 
 if __name__ == "__main__":
     main()
