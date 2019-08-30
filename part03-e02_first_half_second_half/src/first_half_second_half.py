c2602c965a8ce744b86739aef6908ebf092ac8ad
Michal Ungeheuer <wbsth@users.noreply.github.com>
2019-08-30 19:57:20 +0200

    completed exercise p3-2

--- part03-e02_first_half_second_half/src/first_half_second_half.py
+++ part03-e02_first_half_second_half/src/first_half_second_half.py
@@ -3,7 +3,9 @@
 import numpy as np
 
 def first_half_second_half(a):
-    return np.array([])
+    mid = int(a.shape[1] / 2)
+    b = np.sum(a[:,:mid], 1) > np.sum(a[:,mid:], 1)
+    return a[b]
 
 def main():
     pass
