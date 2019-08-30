85eee9ef194a61b561cfe12a1e66c4e856a6bf1f
Michal Ungeheuer <wbsth@users.noreply.github.com>
2019-08-31 00:02:07 +0200

    completed exercise p3-3

--- part03-e03_most_frequent_first/src/most_frequent_first.py
+++ part03-e03_most_frequent_first/src/most_frequent_first.py
@@ -3,7 +3,15 @@
 import numpy as np
 
 def most_frequent_first(a, c):
-    return np.array([])
+    # getting unique values from sorting column
+    uniq = np.unique(a[:,c], axis = 0, return_counts=True)
+    uniq_values,uniq_count = uniq
+    count_sorted_ind = np.argsort(-uniq_count)
+    # getting sorting column values sorted by their numb of occurencies
+    val_sorted_by_count = uniq_values[count_sorted_ind].reshape((1,-1))
+    # getting array of indexes of that sorted array
+    indxs = np.concatenate([np.where((a[:,c] == x))[0] for x in np.nditer(val_sorted_by_count)])
+    return a[indxs]
 
 def main():
     pass
