f7afba1cd071fa91708c0a3511e87e1ecc62b506
Michal Ungeheuer <wbsth@users.noreply.github.com>
2019-09-08 23:45:01 +0200

    completed exercise p3-7

--- part03-e07_meeting_planes/src/meeting_planes.py
+++ part03-e07_meeting_planes/src/meeting_planes.py
@@ -3,7 +3,9 @@
 import numpy as np
 
 def meeting_planes(a1, b1, c1, a2, b2, c2, a3, b3, c3):
-    return []
+    a = np.array([[b1, a1, -1], [b2, a2, -1], [b3, a3, -1]])
+    b = np.array([-c1, -c2, -c3])
+    return np.linalg.solve(a,b)
 
 def main():
     a1=1
