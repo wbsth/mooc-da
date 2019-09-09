b24bad62c1ed7b51e7e5829d94db2f3476f4ab49
Michal Ungeheuer <wbsth@users.noreply.github.com>
2019-09-09 19:19:19 +0200

    completed exercise p3-10

--- part03-e10_subfigures/src/subfigures.py
+++ part03-e10_subfigures/src/subfigures.py
@@ -4,10 +4,14 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 def subfigures(a):
-    pass
+    fig, ax = plt.subplots(1,2)
+    ax[0].plot(a[:,0], a[:,1])
+    ax[1].scatter(a[:,0], a[:,1], s=a[:,3],c=a[:,2])
+    plt.show()
 
 def main():
-    pass
+    a = np.array([[11,3,25,3], [33,14,15,3], [4,6,8,3], [50,50,50,50]])
+    subfigures(a)
 
 if __name__ == "__main__":
     main()
