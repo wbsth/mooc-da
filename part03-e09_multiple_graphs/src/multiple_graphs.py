7062776398f72a21eeec278c61e3e88fd4866867
Michal Ungeheuer <wbsth@users.noreply.github.com>
2019-09-09 18:47:23 +0200

    completed exercise p3-9

--- part03-e09_multiple_graphs/src/multiple_graphs.py
+++ part03-e09_multiple_graphs/src/multiple_graphs.py
@@ -1,9 +1,19 @@
 #!/usr/bin/env python3
 
 import matplotlib.pyplot as plt
+import numpy as np
 
 def main():
-    pass
+    x1 = np.array([2,4,6,7])
+    y1 = np.array([4,3,5,1])
+    x2 = np.array([1,2,3,4])
+    y2 = np.array([4,2,3,1])
 
+    plt.plot(x1, y1)
+    plt.plot(x2, y2)
+    plt.title('plot')
+    plt.xlabel('x-axis')
+    plt.ylabel('y-axis')
+    plt.show()
 if __name__ == "__main__":
     main()
