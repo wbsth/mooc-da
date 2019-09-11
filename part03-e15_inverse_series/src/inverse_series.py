799edb252d6c080221037ed5f5af82c74514354d
Michal Ungeheuer <wbsth@users.noreply.github.com>
2019-09-11 16:33:52 +0200

    completed exercise p3-15

--- part03-e15_inverse_series/src/inverse_series.py
+++ part03-e15_inverse_series/src/inverse_series.py
@@ -3,10 +3,11 @@
 import pandas as pd
 
 def inverse_series(s):
-    return pd.Series()
+    return pd.Series(s.index.values, s.values)
 
 def main():
-    return
+    series = pd.Series(['a','b','c'])
+    print(inverse_series(series))
 
 if __name__ == "__main__":
     main()
