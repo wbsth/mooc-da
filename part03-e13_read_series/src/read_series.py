048e541c018833ded587b7b14cc04ebd3fe5fc1a
Michal Ungeheuer <wbsth@users.noreply.github.com>
2019-09-11 16:08:24 +0200

    completed exercise p3-13

--- part03-e13_read_series/src/read_series.py
+++ part03-e13_read_series/src/read_series.py
@@ -1,11 +1,25 @@
 #!/usr/bin/env python3
-
+import pandas as pd
 
 def read_series():
-    return pd.Series()
+    values = []
+    indexes = []
+    while True:
+        data = input("Dej dane: ")
+        if len(data) == 0:
+            break
+        else: 
+            splitted = data.split()
+            if len(splitted) < 2:
+                raise Exception
+            else:
+                indexes.append(splitted[0])
+                values.append("".join(splitted[1:]))
+
+    return pd.Series(values, indexes)
 
 def main():
-    pass
+    read_series()
 
 if __name__ == "__main__":
     main()
