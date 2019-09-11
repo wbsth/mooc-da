939885023f6db42aa34648506b287e0dafaa8951
Michal Ungeheuer <wbsth@users.noreply.github.com>
2019-09-11 16:26:58 +0200

    completed exercise p3-14

--- part03-e14_operations_on_series/src/operations_on_series.py
+++ part03-e14_operations_on_series/src/operations_on_series.py
@@ -1,14 +1,23 @@
 #!/usr/bin/env python3
-
+import pandas as pd
 
 def create_series(L1, L2):
-    return (None, None)
+    indices = ['a','b','c']
+    s1 = pd.Series(L1, indices)
+    s2 = pd.Series(L2, indices)
+    return (s1, s2)
     
 def modify_series(s1, s2):
-    return (None, None)
+    s1['d'] = s2['b']
+    del s2['b']
+    return (s1, s2)
     
 def main():
-    return
-    
+    L1 = [1,2,3]
+    L2 = [4,5,6]
+    s1, s2 = create_series(L1, L2)
+    s1mod, s2mod = modify_series(s1,s2)
+    print(s1mod + s2mod)
+
 if __name__ == "__main__":
     main()
