55f24ee5407b23943eab2a81efd4b36f83a73339
Michal Ungeheuer <wbsth@users.noreply.github.com>
2019-09-23 19:29:48 +0200

    completed exercise p4-7

--- part04-e07_subsetting_with_loc/src/subsetting_with_loc.py
+++ part04-e07_subsetting_with_loc/src/subsetting_with_loc.py
@@ -3,10 +3,12 @@
 import pandas as pd
 
 def subsetting_with_loc():
-    return None
+    df = pd.read_csv("src/municipal.tsv", sep = '\t', index_col=0)
+    col = df.columns
+    return df.loc['Akaa':'Äänekoski'][[col[0], col[2], col[3]]]
 
 def main():
-    return
+    print(subsetting_with_loc(df))
 
 if __name__ == "__main__":
     main()
