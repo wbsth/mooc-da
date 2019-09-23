f58adb82a1c0a58d19522362285616681a105e53
Michal Ungeheuer <wbsth@users.noreply.github.com>
2019-09-23 18:57:46 +0200

    completed exercise p4-6

--- part04-e06_growing_municipalities/src/growing_municipalities.py
+++ part04-e06_growing_municipalities/src/growing_municipalities.py
@@ -3,10 +3,18 @@
 import pandas as pd
 
 def growing_municipalities(df):
-    0.0
+    all = df.shape[0]
+    col = df.columns
+    filtered = df[df[col[1]] > 0].index
+    part = (len(filtered) / all) 
+    return(part)
 
 def main():
-    return
+    df = pd.read_csv("src/municipal.tsv", sep = '\t', index_col=0)
+    data = df[1:312]
+    percentage = growing_municipalities(data) * 100
+    print(percentage)
+    print(f"Proportion of growing municipalities: {percentage:.1f}%")
 
 if __name__ == "__main__":
     main()
