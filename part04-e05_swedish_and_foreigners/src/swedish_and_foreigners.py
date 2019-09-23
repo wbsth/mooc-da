8f735782e0be3cee0309f54b272e9559771da103
Michal Ungeheuer <wbsth@users.noreply.github.com>
2019-09-23 17:24:08 +0200

    completed exercise p4-5

--- part04-e05_swedish_and_foreigners/src/swedish_and_foreigners.py
+++ part04-e05_swedish_and_foreigners/src/swedish_and_foreigners.py
@@ -3,10 +3,15 @@
 import pandas as pd
 
 def swedish_and_foreigners():
-    return None
+    df = pd.read_csv("src/municipal.tsv", sep = '\t', index_col=0)
+    data = df[1:312]
+    cols = data.columns
+    filtered = data[(data[cols[2]] > 5) & (data[cols[3]] > 5)]
+    print(cols)
+    return filtered[[cols[0], cols[2], cols[3]]]
 
 def main():
-    return
+    print(swedish_and_foreigners())
 
 if __name__ == "__main__":
     main()
