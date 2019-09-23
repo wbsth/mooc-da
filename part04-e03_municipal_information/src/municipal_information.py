2deb787d3e952503f5ebd22dae9ca26376670089
Michal Ungeheuer <wbsth@users.noreply.github.com>
2019-09-23 13:40:13 +0200

    completed exercise p4-3

--- part04-e03_municipal_information/src/municipal_information.py
+++ part04-e03_municipal_information/src/municipal_information.py
@@ -3,8 +3,12 @@
 import pandas as pd
 
 def main():
-    return
-
+    df = pd.read_csv("src/municipal.tsv", sep='\t')
+    df_shape = df.shape
+    print(f"Shape: {df_shape[0]},{df.shape[1]}")
+    print("Columns:")
+    for i in df.columns:
+        print(i)
 
 if __name__ == "__main__":
     main()
