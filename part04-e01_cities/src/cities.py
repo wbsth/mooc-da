35ab7e15770dc8f3213bebe511d60277fa180c58
Michal Ungeheuer <wbsth@users.noreply.github.com>
2019-09-23 12:30:13 +0200

    completed exercise p4-1

--- part04-e01_cities/src/cities.py
+++ part04-e01_cities/src/cities.py
@@ -1,12 +1,22 @@
 #!/usr/bin/env python3
 
 import pandas as pd
+import numpy as np
 
 def cities():
-    return None
+    helsinki = ['Helsinki', 643272, 715.48]
+    espoo = ['Espoo', 279044, 528.03]
+    tampere = ['Tampere', 231853, 689.59]
+    vantaa = ['Vantaa', 223027, 240.35]
+    oulu = ['Oulu', 201810, 3817.52]
+    
+    cities_values = np.array([x[1:] for x in [helsinki, espoo, tampere, vantaa, oulu]])
+    cities_names = np.array([x[0] for x in [helsinki, espoo, tampere, vantaa, oulu]])
+    dataframe = pd.DataFrame(cities_values, columns=['Population', 'Total area'], index=cities_names)
+    return dataframe
     
 def main():
-    return
+    print(cities())
     
 if __name__ == "__main__":
     main()
