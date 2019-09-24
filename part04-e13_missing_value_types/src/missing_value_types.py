dd721e32499dc46bdd94ae364e1d6483e7cb39f8
Michal Ungeheuer <wbsth@users.noreply.github.com>
2019-09-25 00:07:41 +0200

    completed exercise p4-13

--- part04-e13_missing_value_types/src/missing_value_types.py
+++ part04-e13_missing_value_types/src/missing_value_types.py
@@ -4,10 +4,14 @@ import pandas as pd
 import numpy as np
 
 def missing_value_types():
-    return None
-               
+    countries = ['United Kingdom', 'Finland', 'USA', 'Sweden', 'Germany', 'Russia']
+    year_of_inpedence = [None, 1917, 1776,1523,None,1992]
+    presidents = [None, 'Niinistö', 'Trump', None, 'Steinmeier', 'Putin']
+    
+    df = pd.DataFrame({'State':countries, 'Year of independence':year_of_inpedence, 'President':presidents}).set_index('State')
+    return df
 def main():
-    return
-
+    missing_value_types()
+    
 if __name__ == "__main__":
     main()
