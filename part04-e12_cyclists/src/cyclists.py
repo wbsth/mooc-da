b668cdcb946709f8d62eb0fa5cc375ecf78d24d6
Michal Ungeheuer <wbsth@users.noreply.github.com>
2019-09-24 16:02:54 +0200

    completed exercise p4-12

--- part04-e12_cyclists/src/cyclists.py
+++ part04-e12_cyclists/src/cyclists.py
@@ -3,11 +3,14 @@
 import pandas as pd
 
 def cyclists():
-    return None
-
+    df = pd.read_csv('src/Helsingin_pyorailijamaarat.csv', sep=";")
+    df_droprow = df.dropna(how = "all")
+    df_dropcol = df_droprow.dropna(axis = 1, how = "all")
+    return df_dropcol
 
 def main():
-    return
+    print(cyclists())
+
     
 if __name__ == "__main__":
     main()
