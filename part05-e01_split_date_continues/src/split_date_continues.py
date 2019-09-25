0a2f1458164655013b23e2099e22a34e02c22a07
Michal Ungeheuer <wbsth@users.noreply.github.com>
2019-09-25 22:08:32 +0200

    part 5 exercises

---
+++ part05-e01_split_date_continues/src/split_date_continues.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+
+import pandas as pd
+
+
+def split_date(df):
+    d = df["Päivämäärä"].str.split(expand=True)
+    d.columns = ["Weekday", "Day", "Month", "Year", "Hour"]
+
+    hourmin = d["Hour"].str.split(":", expand=True)
+    d["Hour"] = hourmin.iloc[:, 0]
+
+    d["Weekday"] = d["Weekday"].map(days)
+    d["Month"] = d["Month"].map(months)
+    
+    d = d.astype({"Weekday": object, "Day": int, "Month": int, "Year": int, "Hour": int})
+    return d
+
+def split_date_continues():
+    return None
+
+def main():
+    df = split_date_continues()
+    print("Shape:", df.shape)
+    print("Column names:\n", df.columns)
+    print(df.head())
+
+
+if __name__ == "__main__":
+    main()
