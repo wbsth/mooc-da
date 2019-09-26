22e4e7a05370e827d777ed2493e332d150559082
Michal Ungeheuer <wbsth@users.noreply.github.com>
2019-09-26 15:51:16 +0200

    completed exercise p5-1

--- part05-e01_split_date_continues/src/split_date_continues.py
+++ part05-e01_split_date_continues/src/split_date_continues.py
@@ -17,7 +17,54 @@ def split_date(df):
     return d
 
 def split_date_continues():
-    return None
+    # reading data from csv
+    df = pd.read_csv('src/Helsingin_pyorailijamaarat.csv', sep=";")
+
+    # dropping unecessary rows and columns
+    df = df.dropna(how = "all")
+    df = df.dropna(axis = 1, how = "all")
+
+    # extracting first column to separate var
+    col = df.iloc[:,0] 
+    df = df.drop(df.columns[0], axis=1) 
+
+    # splitting data, translating
+    col = col.str.split(expand = True)
+    col.columns = ["Weekday", "Day", "Month", "Year", "Hour"]
+    col['Hour'] = col['Hour'].str[0:2]
+    
+    weekdays = {
+        "ma":"Mon",
+        "ti":"Tue",
+        "ke":"Wed",
+        "to":"Thu",
+        "pe":"Fri",
+        "la":"Sat",
+        "su":"Sun"
+    }
+    
+    months = {
+        "tammi" :1,
+        "helmi" :2,
+        "maalis" :3,
+        "huhti" :4,
+        "touko" :5,
+        "kesä":6,
+        "heinä" :7,
+        "elo" :8,
+        "syys" :9,
+        "loka" :10,
+        "marras" :11,
+        "joulu" :12,
+    }
+
+    col["Weekday"] = col["Weekday"].map(weekdays)
+    col["Month"] = col["Month"].map(months)
+    col = col.astype({"Weekday": object, "Day": int, "Month": int, "Year": int, "Hour": int})
+
+    res = pd.concat([col, df], axis = 1)
+
+    return res
 
 def main():
     df = split_date_continues()
