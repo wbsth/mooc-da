c25d003b8068ff228e88c842648a168ce9ad5811
Michal Ungeheuer <wbsth@users.noreply.github.com>
2019-10-06 17:29:48 +0200

    completed exercise p5-5

--- part05-e05_best_record_company/src/best_record_company.py
+++ part05-e05_best_record_company/src/best_record_company.py
@@ -3,10 +3,18 @@
 import pandas as pd
 
 def best_record_company():
-    return None
+    top = pd.read_csv('src/UK-top40-1964-1-2.tsv', sep='\t')
+    test = top.groupby('Publisher')
+    a = test['WoC'].sum()
+    a = a.sort_values(ascending=False)
+    top_publisher = a.index[0]
+    final_df = top[top.Publisher == top_publisher]
+    return final_df
+
 
 def main():
-    return
+    print(best_record_company())
+
     
 
 if __name__ == "__main__":
