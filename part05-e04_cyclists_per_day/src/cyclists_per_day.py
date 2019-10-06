7ffd99009d9dcb7793a6590eed988273b0a5fc51
Michal Ungeheuer <wbsth@users.noreply.github.com>
2019-10-06 16:57:46 +0200

    completed exercise p5-4

--- part05-e04_cyclists_per_day/src/cyclists_per_day.py
+++ part05-e04_cyclists_per_day/src/cyclists_per_day.py
@@ -4,11 +4,70 @@ import pandas as pd
 import matplotlib.pyplot as plt
 
 
+def split_date_continues():
+    # reading data from csv
+    df = pd.read_csv('src/Helsingin_pyorailijamaarat.csv', sep=";")
+
+    # dropping unecessary rows and columns
+    df = df.dropna(how="all")
+    df = df.dropna(axis=1, how="all")
+
+    # extracting first column to separate var
+    col = df.iloc[:, 0]
+    df = df.drop(df.columns[0], axis=1)
+
+    # splitting data, translating
+    col = col.str.split(expand=True)
+    col.columns = ["Weekday", "Day", "Month", "Year", "Hour"]
+    col['Hour'] = col['Hour'].str[0:2]
+
+    weekdays = {
+        "ma": "Mon",
+        "ti": "Tue",
+        "ke": "Wed",
+        "to": "Thu",
+        "pe": "Fri",
+        "la": "Sat",
+        "su": "Sun"
+    }
+
+    months = {
+        "tammi": 1,
+        "helmi": 2,
+        "maalis": 3,
+        "huhti": 4,
+        "touko": 5,
+        "kesä": 6,
+        "heinä": 7,
+        "elo": 8,
+        "syys": 9,
+        "loka": 10,
+        "marras": 11,
+        "joulu": 12,
+    }
+
+    col["Weekday"] = col["Weekday"].map(weekdays)
+    col["Month"] = col["Month"].map(months)
+    col = col.astype({"Weekday": object, "Day": int, "Month": int, "Year": int, "Hour": int})
+
+    res = pd.concat([col, df], axis=1)
+
+    return res
+
+
 def cyclists_per_day():
-    return None
-    
+    df = split_date_continues()
+    df = df.drop(['Hour', 'Weekday'], axis=1)
+    new_df = df.groupby(['Year', 'Month', 'Day']).sum()
+    return new_df
+
+
 def main():
-    return
+    df = cyclists_per_day()
+    df_plot = df.loc[(2017, 8), :]
+    plt.plot(df_plot)
+    plt.show()
+
 
 if __name__ == "__main__":
     main()
