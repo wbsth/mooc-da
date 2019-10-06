0c1351793fb3f7f297e3efcfd7e21e51f186162b
Michal Ungeheuer <wbsth@users.noreply.github.com>
2019-10-06 23:41:23 +0200

    completed exercise p5-6

--- part05-e06_suicide_fractions/src/suicide_fractions.py
+++ part05-e06_suicide_fractions/src/suicide_fractions.py
@@ -3,10 +3,14 @@
 import pandas as pd
 
 def suicide_fractions():
-    return pd.Series()
+    df = pd.read_csv('src/who_suicide_statistics.csv')
+    df['suicide_ratio'] = df['suicides_no']/df['population']
+    final_df = df.groupby('country')
+    final_df = final_df['suicide_ratio'].mean()
+    return final_df
 
 def main():
-    return
+    suicide_fractions()
 
 if __name__ == "__main__":
     main()
