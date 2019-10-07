58a157c3c9644f12af5af9e92ea6f70e73046c56
Michal Ungeheuer <wbsth@users.noreply.github.com>
2019-10-07 21:17:40 +0200

    completed exercise p5-8

--- part05-e08_bicycle_timeseries/src/bicycle_timeseries.py
+++ part05-e08_bicycle_timeseries/src/bicycle_timeseries.py
@@ -2,13 +2,68 @@
 
 import pandas as pd
 
+def split_date_continues():
+    # reading data from csv
+    df = pd.read_csv('src/Helsingin_pyorailijamaarat.csv', sep=";")
+
+    # dropping unecessary rows and columns
+    df = df.dropna(how="all")
+    df = df.dropna(axis=1, how="all")
+
+    # extracting first column to separate var
+    col = df.iloc[:, 0]
+    df = df.drop(df.columns[0], axis=1)
+
+    # splitting data, translating
+    col = col.str.split(expand=True)
+    col.columns = ["Weekday", "Day", "Month", "Year", "Hour"]
+    col['Hour'] = col['Hour'].str[0:2]
+
+    weekdays = {
+        "ma": "Mon",
+        "ti": "Tue",
+        "ke": "Wed",
+        "to": "Thu",
+        "pe": "Fri",
+        "la": "Sat",
+        "su": "Sun"
+    }
+
+    months = {
+        "tammi": 1,
+        "helmi": 2,
+        "maalis": 3,
+        "huhti": 4,
+        "touko": 5,
+        "kesä": 6,
+        "heinä": 7,
+        "elo": 8,
+        "syys": 9,
+        "loka": 10,
+        "marras": 11,
+        "joulu": 12,
+    }
+
+    col["Weekday"] = col["Weekday"].map(weekdays)
+    col["Month"] = col["Month"].map(months)
+    col = col.astype({"Weekday": object, "Day": int, "Month": int, "Year": int, "Hour": int})
+
+    res = pd.concat([col, df], axis=1)
+
+    return res
+
 
 def bicycle_timeseries():
-    return pd.DataFrame()
+    df = split_date_continues()
+    df["Date"] = pd.to_datetime(df[["Day", "Month", "Year", 'Hour']], dayfirst=True)
+    df = df.drop(['Day', 'Month', 'Year', 'Hour', 'Weekday'], axis=1)
+    df = df.set_index("Date")
+    return df
 
 
 def main():
-    return None
+    print(bicycle_timeseries())
+
 
 if __name__ == "__main__":
     main()
