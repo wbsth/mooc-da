9db7342b4859dddd1fb06fae38e0efe3da4d5536
Michal Ungeheuer <wbsth@users.noreply.github.com>
2019-10-08 13:06:51 +0200

    completed exercise p5-10

--- part05-e10_linear_regression/src/linear_regression.py
+++ part05-e10_linear_regression/src/linear_regression.py
@@ -5,9 +5,29 @@ from sklearn.linear_model import LinearRegression
 import matplotlib.pyplot as plt
 
 def fit_line(x, y):
-    return 0, 0
+    x = x[:, np.newaxis]
+    model = LinearRegression(fit_intercept=True)
+    model.fit(x, y)
+
+    xfit = np.linspace(0, 10, 20)[:,np.newaxis]
+    yfit = model.predict(xfit)
+
+    return model.coef_[0], model.intercept_
     
 def main():
-    
+    n = 20
+    x = np.linspace(0, 10, n)
+    y = x**2 + 2 * np.random.randn(n)
+    coef, intercept = fit_line(x, y)
+
+    print(f"Slope: {coef}")
+    print(f"Intercept: {intercept}")
+
+    yfit = coef*x + intercept
+    plt.plot(x, y, 'o')
+    plt.plot(x, yfit)
+    plt.show()
+
+
 if __name__ == "__main__":
     main()
