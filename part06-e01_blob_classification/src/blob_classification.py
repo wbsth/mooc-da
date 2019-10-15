ab32d3f3d6737428b35aa15929c0c4e29ecea2e4
Michal Ungeheuer <wbsth@users.noreply.github.com>
2019-10-15 13:06:51 +0200

    completed exercise p6-1

--- part06-e01_blob_classification/src/blob_classification.py
+++ part06-e01_blob_classification/src/blob_classification.py
@@ -7,7 +7,16 @@ from sklearn import datasets
 from sklearn.model_selection import train_test_split
 
 def blob_classification(X, y):
-    return 0.0
+    # x - feature matrix
+    # y - label vector
+    # return accuracy score
+    xtrain, xtest, ytrain, ytest = train_test_split(X, y, random_state=0, train_size=0.75)
+    model = GaussianNB()
+    model.fit(xtrain, ytrain)
+    y_predicted = model.predict(xtest)
+    score = metrics.accuracy_score(ytest, y_predicted)
+
+    return score
 
 def main():
     X,y = datasets.make_blobs(100, 2, centers=2, random_state=2, cluster_std=2.5)
