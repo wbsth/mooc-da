c4d7dfb3dca7cf3bc4669c80c6f3afc5fc6d83fd
Michal Ungeheuer <wbsth@users.noreply.github.com>
2019-10-15 13:18:10 +0200

    completed exercise p6-2

--- part06-e02_plant_classification/src/plant_classification.py
+++ part06-e02_plant_classification/src/plant_classification.py
@@ -6,7 +6,13 @@ from sklearn import naive_bayes
 from sklearn import metrics
 
 def plant_classification():
-    return 0.0
+    iris = load_iris()
+    xtrain, xtest, ytrain, ytest = train_test_split(iris.data, iris.target, train_size=0.8, random_state=0)
+    model = naive_bayes.GaussianNB()
+    model.fit(xtrain, ytrain)
+    y_predict = model.predict(xtest)
+    score = metrics.accuracy_score(ytest, y_predict)
+    return score
 
 def main():
     print(f"Accuracy is {plant_classification()}")
